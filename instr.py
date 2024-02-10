d638cbb36e9046137ea5825a936c928a33d701d6
ErrorTop1GG <158756334+ErrorTop1GG@users.noreply.github.com>
2024-02-10 18:24:06 +0300

    0.02v
    переменные

--- instr.py
+++ instr.py
@@ -2,26 +2,26 @@
 win_x, win_y = 200, 100
 win_width, win_height = 1000, 600
 
-txt_hello = 'Добро пожаловать в программу по определению состояния здоровья!'
-txt_next = 'Начать'
-txt_instruction = 'magic' 
-txt_title = 'Здоровье'
-txt_name = 'Введите Ф.И.О.:'
-txt_hintname = "Ф.И.О."
-txt_hintage = "0"
-txt_test1 = ''
-txt_test2 = ''
-txt_test3 = 'k'
-txt_sendresults = 'Отправить результаты'
+txt_hello = 'Привет это тест Руфье'
+txt_next = 'Это тест Руфье, проходим'
+txt_instruction = '' 
+txt_title = ''
+txt_name = 'Введите Ф.И.О'
+txt_age='Введите возраст'
+txt_hintname = ""
+txt_hintage = ""
+txt_test1 = 'Ляг'
+txt_test2 = 'Присядай'
+txt_test3 = 'Отдыхай'
+txt_sendresults = 'Показать на сколько ты стар'
 txt_hinttest1 = '0'
 txt_hinttest2 = '0'
 txt_hinttest3 = '0'
-txt_starttest1 = 'Начать первый тест'
-txt_starttest2 = 'Начать делать приседания'
-txt_starttest3 = 'Начать финальный тест'
-txt_timer = 'k'
- 
-txt_age = 'Полных лет:'
-txt_finalwin = 'Результаты'
-txt_index = 'Индекс Руфье: '
-txt_workheart = 'Работоспособность сердца: '
+txt_starttest1 = ''
+txt_starttest2 = ''
+txt_starttest3 = ''
+
+txt_age = ''
+txt_finalwin = ''
+txt_index = ''
+txt_workheart = ''
